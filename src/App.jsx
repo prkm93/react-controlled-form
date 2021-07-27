d17bbf30f89f8b0ddeb8c88dbaa85ba04436704a
prkm93 <aradshub93@gmail.com>
2021-07-27 22:06:52 +0530

    integrated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Checkbox from './components/Checkbox'
+import Dropdown from './components/Dropdown'
+import Input from './components/Input'
+import InputNumber from './components/InputNumber'
+import Radio from './components/Radio'
+import Textbox from './components/Textbox'
 
 function App() {
     return (
         <div>
             <h1>ReactJs Controlled Form Component</h1>
+            <Checkbox/>
+            <Dropdown/>
+            <Input/>
+            <InputNumber/>
+            <Radio/>
+            <Textbox/>
         </div>
     )
 }
