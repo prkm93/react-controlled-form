dba220a63cfdbf593bde69ad91f2c54b732a6fc6
prkm93 <aradshub93@gmail.com>
2021-07-27 22:04:36 +0530

    added components

---
+++ src/components/InputNumber.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputNumber() {
+    return (
+        <div>
+            Input Number
+        </div>
+    )
+}
+
+export default InputNumber
---
+++ src/components/Textbox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Textbox() {
+    return (
+        <div>
+            Textbox
+        </div>
+    )
+}
+
+export default Textbox
