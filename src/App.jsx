821e1a9fcd3f03c867234b2a74570b6028430060
prkm93 <aradshub93@gmail.com>
2021-08-03 21:46:20 +0530

    updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Input from './components/Input'
 import InputNumber from './components/InputNumber'
 import Radio from './components/Radio'
 import Textbox from './components/Textbox'
+import './style.css'
 
 function App() {
     return (
-        <div>
-            <h1>ReactJs Controlled Form Component</h1>
-            <Checkbox/>
-            <Dropdown/>
-            <Input/>
-            <InputNumber/>
-            <Radio/>
-            <Textbox/>
+        <div className="container">
+            <h1 className="text-center">ReactJs Controlled Form Component</h1>
+            <div className="box">
+                <h3>Pet Adoption Form</h3>
+                <Input/>
+                <Dropdown/>
+                <Checkbox/>
+                <Radio/>
+                <InputNumber/>
+                <Textbox/>
+            </div>
         </div>
     )
 }
