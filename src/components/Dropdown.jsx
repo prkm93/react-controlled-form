08492a45cdcbbb0906c9e49fe25d60e53e66e9b9
prkm93 <aradshub93@gmail.com>
2021-08-05 09:18:47 +0530

    added props for Dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from 'react';
+import PropTyes from 'prop-types';
 
-function Dropdown() {
+function Dropdown({title, name, options, value, controlFunc}) {
     return (
         <div className="form-group mt-2 w-50">
-            <select className="form-select mb-2" aria-label="Default select example">
-                <option selected>Choose your age group:</option>
-                <option value="1">18-25</option>
-                <option value="2">25-59</option>
-                <option value="3">60 or older</option>
+            <select 
+                className="form-select mb-2" 
+                aria-label="Default select example"
+                name={name}
+                value={value}
+                onChange={controlFunc}
+                >
+                <option selected>{title}</option>
+                {
+                    options && options.length && options.map((option) => {
+                        return <option value={option} key={option}>{option}</option>
+                    })
+                }
             </select>
         </div>
     )
+}
+
+Dropdown.PropTyes = {
+    title: PropTyes.string.isRequired, 
+    name: PropTyes.string.isRequired,
+    options: PropTyes.array.isRequired,
+    value: PropTyes.string.isRequired,
+    controlFunc: PropTyes.func.isRequired
 }
 
 export default Dropdown
--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -9,17 +9,23 @@ import Textbox from './Textbox'
 function FormContainer() {
 
     const [ownerName, setOwnerName] = useState('');
-    const [ageRange, setAgeRange] = useState([]);
+    const [ageRange] = useState(['18-25', '25-59', '60 or older']);
+    const [selectedAgeRange, setSelectedAgeRange] = useState('');
+    const [pets] = useState(['Dog', 'Rabbit', 'Pony', 'Fish', 'Cat', 'Iguana', 'Mouse', 'Bird', 'Cow']);
     const [adoptPet, setAdoptPet] = useState([]);
     const [siblingOptions, setSiblingOptions] = useState('');
     const [currentOwnedPets, setCurrentOwnedPets] = useState(0);
     const [petDetails, setPetDetails] = useState('');
 
 
+    const handleFormSubmit = () => {
+        console.log("submit");
+    }
+
     return (
         <div>
             <h3>Pet Adoption Form</h3>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <Input
                     title={'Full Name'}
                     inputType={'text'}
@@ -28,8 +34,16 @@ function FormContainer() {
                     value={ownerName}
                     controlFunction={e => setOwnerName(e.target.value)}
                 />
-                <Dropdown/>
-                <Checkbox/>
+                <Dropdown
+                    title={'Choose your age group:'}
+                    name={'ageRange'}
+                    options={ageRange}
+                    value={selectedAgeRange}
+                    controlFunc={e => setSelectedAgeRange(e.target.value)}
+                />
+                <Checkbox
+                      
+                />
                 <Radio/>
                 <InputNumber/>
                 <Textbox/>
